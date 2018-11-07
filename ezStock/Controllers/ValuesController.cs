5640d1d274d96ee6cec36430ee5eaec4f0113522
weiting <a26007565@gmail.com>
2018-11-07 09:46:06 +0800

    增加CodePages才能使用big5編碼

--- ezStock/Controllers/ValuesController.cs
+++ ezStock/Controllers/ValuesController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ezStock.Controllers
@@ -12,9 +16,11 @@ namespace ezStock.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<Stock> Get()
         {
-            return new string[] { "value1", "value2" };
+            var util = new StockUtil();
+            
+            return util.GetStockList();
         }
 
         // GET api/values/5
@@ -40,6 +46,57 @@ namespace ezStock.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+    }
+
+    public class Stock
+    {
+
+        public string Id;
+        public string Name;
+
+        public Stock(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class StockUtil
+    {
+        // 本國上市證券國際證券辨識號碼一覽表
+        private const string _url2 = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=2";
+        // 本國上櫃證券國際證券辨識號碼一覽表
+        private const string _url4 = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=4";
+
+        // TODO: 要存在JSON定期更新，避免記憶體使用過大
+        public IEnumerable<Stock> GetStockList()
+        {
+            //var doc = new HtmlWeb
+            //{
+            //    AutoDetectEncoding = false,
+            //    OverrideEncoding = Encoding.Default,
+            //}.Load(_url);
+            IEnumerable<Stock> list2 = GetOfficeStockList(_url2);
+            IEnumerable<Stock> list4 = GetOfficeStockList(_url4);
+          
+            return list2.Concat(list4);
+        }
+
+        private static IEnumerable<Stock> GetOfficeStockList(string url)
+        {
+            //指定來源網頁
+            //WebClient url = new WebClient();
+            //將網頁來源資料暫存到記憶體內
+            MemoryStream ms = new MemoryStream(new WebClient().DownloadData(url));
+            // 使用預設編碼讀入 HTML 
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(ms, Encoding.GetEncoding("Big5"));
+            var list = doc.DocumentNode.SelectNodes("//table[@class='h4']/tr[position()>2]/td[1]")
+                .Select(x =>
+                    new Stock(x.InnerText.Split(null)[0], x.InnerText.Split(null)[1])
+                );
+            return list;
         }
     }
 }
--- ezStock/Startup.cs
+++ ezStock/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using System.Text;
 
 namespace ezStock
 {
@@ -17,6 +11,7 @@ namespace ezStock
     {
         public Startup(IConfiguration configuration)
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Configuration = configuration;
         }
 
