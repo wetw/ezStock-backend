ac77f3f2bb70e94beed50316790a420db589a67f
weiting <a26007565@gmail.com>
2019-07-24 14:49:50 +0800

    Add twse api

---
+++ src/ezStock.twse/ExchangeReportServices.cs
@@ -0,0 +1,22 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ezStock.twse
+{
+    public class ExchangeReportServices
+    {
+        private readonly IExchangeReport _exchangeReport;
+        private const string BaseUrl = "https://www.twse.com.tw";
+
+        public ExchangeReportServices()
+        {
+            //_httpClient = new HttpClient(new HttpClientDiagnosticsHandler());
+            _exchangeReport = RestService.For<IExchangeReport>(BaseUrl);
+        }
+
+        public async Task<object> GetAsync(string date, string type = "ALL", string response = "json")
+        {
+            return await _exchangeReport.GetAsync(date, type, response).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/ezStock.twse/IExchangeReport.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ezStock.twse
+{
+    public interface IExchangeReport
+    {
+        // https://www.twse.com.tw/exchangeReport/MI_INDEX?response=json&date=20190709&type=ALL
+        [Get("/exchangeReport/MI_INDEX")]
+        Task<object> GetAsync([Query]string date, [Query]string type = "ALL", [Query]string response = "json");
+    }
+}
